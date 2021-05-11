a8b482510cb281b31e08194806f09f3de38206b7
corey smaller <coreysmaller@gmail.com>
2021-05-11 11:27:47 -0600

    backing tracks

--- src/pages/backing.tsx
+++ src/pages/backing.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import ReactAudioPlayer from 'react-audio-player';
 
+const tracks = [
+    { title: "Adore You", src: "/assets/adore_you.mp3" },
+    { title: "Thrift Shop", src: "/assets/thrift_shop.mp3" },
+    { title: "Uptown Funk", src: "/assets/uptown_funk.mp3" },
+    { title: "Shape Of You", src: "/assets/shape_of_you.mp3" },
+    { title: "Don't", src: "/assets/dont.mp3" },
+
+]
+const Track = ({ src, title }) =>
+    <div className="pt-5 px-5">
+        <h3> {title}</h3>
+        <ReactAudioPlayer
+            src={src}
+            autoPlay={false}
+            controls
+        />
+    </div>
+
 const Backing = () => {
     return (
-        <div className="pt-5 px-5">
-            <h3> Adore You backing tracks (right click to download)</h3>
-            <ReactAudioPlayer
-                src="/assets/adore_you.mp3"
-                autoPlay={false}
-                controls
-            />
+        <div>
+            {tracks.map((track, index) => {
+                <Track key={index} src={track.src} title={track.title} />
+            })}
+
         </div>
     )
 }
