04b52ee8bea4d7afd877dbf7e0f1be78e1a82dc4
Corey Smaller <coreysmaller@Coreys-MacBook-Pro.local>
2021-07-04 10:28:45 -0400

    search

--- src/components/songlist.tsx
+++ src/components/songlist.tsx
@@ -3,7 +3,6 @@ import songList from "../../static/songs/songs";
 import styled from "styled-components";
 import { ArrowDown, ArrowUp } from 'react-bootstrap-icons';
 import { Button } from "react-bootstrap";
-import { sort } from "core-js/core/array";
 
 const SongTable = styled.table`
     .songs-header {
@@ -30,6 +29,7 @@ export default function SongList() {
     const [artistDirectionDown, setArtistDirectionDown] = useState(false);
     const [titleDirectionDown, setTitleDirectionDown] = useState(false);
     const [songListObject, setSongListObject] = useState({ sortBy: "title", sortDown: true, songs: songList.songs });
+    const [search, setSearch] = useState("");
 
     const sortSongs = (name, sortDown) => {
         let ar = songList.songs;
@@ -48,46 +48,80 @@ export default function SongList() {
         sortSongs("title", false);
     }, []);
 
+    useEffect(() => {
+        if (search === "") {
+            setSongListObject({ ...songListObject, ...{ songs: songList.songs } });
+        } else {
+            const items = filterSongList(search);
+            setSongListObject({ ...songListObject, ...{ songs: items } });
+        }
+
+    }, [search])
+
+    function filterSongList(search) {
+        return songList.songs.filter((data) => {
+            if (data.title.toLowerCase().includes(search.toLowerCase()) || data.artist.toLowerCase().includes(search.toLowerCase())) {
+                return data
+            }
+        });
+    }
+
+    function searchSongs(event) {
+        setSearch(event.target.value);
+    }
+
+    function resetSearch() {
+        setSearch("");
+    }
+
     return (
-        <SongTable>
-            <thead className="songs-header">
-                <tr>
-                    <td >
-                        Song
+        <>
+            <div className="form-group d-flex flex-row-reverse">
+                <button className="form-control btn-danger" onClick={resetSearch}>Reset Search</button>
+                <input id="search-text" type="text" className="form-control px-4 mx-2" placeholder="Song / Artist" onChange={(e) => searchSongs(e)} />
+                <label className="pt-2 pr-2" for="search-text">Search: </label>
+            </div>
+
+            <SongTable>
+                <thead className="songs-header">
+                    <tr>
+                        <td >
+                            Song
                         {!titleDirectionDown &&
-                            <Button onClick={() => sortSongs("title", true)}>
-                                <ArrowDown />
-                            </Button>
-                        }
-                        {titleDirectionDown &&
-                            <Button onClick={() => sortSongs("title", false)}>
-                                <ArrowUp />
-                            </Button>
-                        }
-                    </td>
-                    <td>
-                        Artist
+                                <Button onClick={() => sortSongs("title", true)}>
+                                    <ArrowDown />
+                                </Button>
+                            }
+                            {titleDirectionDown &&
+                                <Button onClick={() => sortSongs("title", false)}>
+                                    <ArrowUp />
+                                </Button>
+                            }
+                        </td>
+                        <td>
+                            Artist
                         {!artistDirectionDown &&
-                            <Button onClick={() => sortSongs("artist", true)}>
-                                <ArrowDown />
-                            </Button>
-                        }
-                        {artistDirectionDown &&
-                            <Button onClick={() => sortSongs("artist", false)}>
-                                <ArrowUp />
-                            </Button>
-                        }
-                    </td>
-                </tr>
-            </thead>
-            <tbody>
-                {songListObject.songs.map((song, index) => {
-                    return <tr key={index}>
-                        <td>{song.title}</td>
-                        <td>{song.artist}</td>
+                                <Button onClick={() => sortSongs("artist", true)}>
+                                    <ArrowDown />
+                                </Button>
+                            }
+                            {artistDirectionDown &&
+                                <Button onClick={() => sortSongs("artist", false)}>
+                                    <ArrowUp />
+                                </Button>
+                            }
+                        </td>
                     </tr>
-                })}
-            </tbody>
-        </SongTable>
+                </thead>
+                <tbody>
+                    {songListObject.songs.map((song, index) => {
+                        return <tr key={index}>
+                            <td>{song.title}</td>
+                            <td>{song.artist}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </SongTable>
+        </>
     )
 }
