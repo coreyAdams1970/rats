075d5bf1f40a83c792f0140b34ef17ed1f329d5f
corey <csmaller@elumatherapy.com>
2021-11-04 08:10:13 -0600

    fix helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'G-4G7LKPFTXH',
       },
     },
     {
