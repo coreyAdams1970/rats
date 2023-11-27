e0f72766633528c80c0e53c9a43ab8f688373534
corey <csmaller@elumatherapy.com>
2023-11-27 10:03:50 -0700

    change gtag

--- src/html.js
+++ src/html.js
@@ -1,46 +1,48 @@
-import React from "react";
 import PropTypes from "prop-types";
+import React from "react";
 
 export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes}>
-      <head>
+    return (
+        <html {...props.htmlAttributes}>
+            <head>
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-C67R4TN00K"
+                ></script>
+                <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4G7LKPFTXH"></script>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <link
-          type="application/opensearchdescription+xml"
-          rel="search"
-          href="opensearch.xml"
-        />
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-        <script src="netlifyauth.js"></script>
-      </body>
-    </html>
-  );
+                <meta charSet="utf-8" />
+                <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+                <link
+                    type="application/opensearchdescription+xml"
+                    rel="search"
+                    href="opensearch.xml"
+                />
+                {props.headComponents}
+            </head>
+            <body {...props.bodyAttributes}>
+                {props.preBodyComponents}
+                <div
+                    key={`body`}
+                    id="___gatsby"
+                    dangerouslySetInnerHTML={{ __html: props.body }}
+                />
+                {props.postBodyComponents}
+                <script src="netlifyauth.js"></script>
+            </body>
+        </html>
+    );
 }
 
 HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
+    htmlAttributes: PropTypes.object,
+    headComponents: PropTypes.array,
+    bodyAttributes: PropTypes.object,
+    preBodyComponents: PropTypes.array,
+    body: PropTypes.string,
+    postBodyComponents: PropTypes.array,
 };
